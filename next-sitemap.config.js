d15eafbc19173e1aacd69eb274d768e49b233a22
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-05-19 12:43:25 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next-sitemap').IConfig} */
+/* @type {import('next-sitemap').IConfig} 
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://solarscript.com.ng',
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
+*/
